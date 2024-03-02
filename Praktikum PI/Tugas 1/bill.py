77b7daca893cba93c33d69775766744e358d1c5f
billclinton <bill.cinton2804@gmail.com>
2024-03-02 13:56:48 +0700

    edit

---
+++ Praktikum PI/Tugas 1/bill.py
@@ -0,0 +1,20 @@
+#Inputan untuk nama dan nim
+nama = input("Masukkan Nama Anda : ")
+nim = int(input("Masukkan NIM Anda : "))
+
+#Output inputan nama dan nim
+'\n'
+'\n'
+
+print(nama )
+print(nim)
+
+'\n'
+'\n'
+
+#Inputan Untuk Angka
+angka1 = int(input("Masukkan Angka Pertama : "))
+angka2 = int(input("Masukkan Angka Kedua :"))
+
+
+
